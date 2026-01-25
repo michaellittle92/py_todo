0aaef02fa242244b3be35634826376a7f8bad47b
Michael <michaellittle92@gmail.com>
2026-01-25 15:23:26 +1100

    added endpoint for getting single result back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path, status
 import models
 from models import Todos
 from db import engine, SessionLocal
@@ -18,7 +18,16 @@ def get_db():
     finally:
         db.close()
 
+db_dependancy = Annotated[Session, Depends(get_db)]
 
-@app.get("/")
-async def read_all(db: Annotated[Session, Depends(get_db)]):
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependancy):
     return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependancy, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found")
