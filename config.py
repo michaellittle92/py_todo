5b98fe5fac2214adf2afaa385350687fdc33d5d7
Michael <michaellittle92@gmail.com>
2026-02-01 09:56:56 +1100

    migrate db from sqlite to postgres

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     SECRET_KEY: str
+    CONNECTION_STRING: str
 
     class Config:
         env_file = ".env"
--- db.py
+++ db.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from config import settings
 
 from sqlalchemy.ext.declarative import declarative_base
 
-SLQALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SLQALCHEMY_DATABASE_URL = settings.CONNECTION_STRING
 
-engine = create_engine(SLQALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SLQALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
